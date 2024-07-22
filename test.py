be59785d2323843402db2bd54a56b93e077b65d9
hhhhhhhhhhhhhhhhhhhhhhhhhugooooooo <122105420+hhhhhhhhhhhhhhhhhhhhhhhhhugooooooo@users.noreply.github.com>
2024-07-22 20:10:11 +0200

    changed ssh command

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import os
 
 def main():
     print("Hello World!")
-    ssh_command= "ssh 172.17.0.1 ls -l"
+    ssh_command= "ssh 172.17.0.1 echo 'Hello World!'"
     os.popen(ssh_command).read().rstrip()
     print("Done!")
     
